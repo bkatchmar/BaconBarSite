2e47b8ad8dccddebfed8d9c6e429515061ecdf5e
Brian Katchmar <brian.katchmar@gmail.com>
2013-10-21 21:35:50 -0400

    Gallery Finally Done

--- js/denihan.baconbar.page.load.js
+++ js/denihan.baconbar.page.load.js
@@ -5,22 +5,42 @@
         target: "header#primary nav div#about_dropdown_section",
         additionalTrigger: function (on, off, target, $trigger) {
             $trigger.mouseleave(function (e) {
-                var targetId = e.toElement.id;
-
-                if (targetId != "about_dropdown_section") {
+                var rolledTo = jQuery(e.toElement);
+                if (rolledTo) { rolledTo = jQuery(e.relatedTarget) }
+                if (rolledTo.attr("class") != "sub_section" && rolledTo.attr("class") != "item" && rolledTo.attr("class") != "dropdownTrigger") {
                     off($trigger, target);
                 }
             });
 
             jQuery(target).mouseleave(function (e) {
-                var targetId = e.toElement.id;
-
-                if (targetId != "about_dropdown" && targetId != "about_menu") {
+                var rolledTo = jQuery(e.toElement);
+                if (rolledTo) { rolledTo = jQuery(e.relatedTarget) }
+                if (rolledTo.attr("class") != "sub_section" && rolledTo.attr("class") != "item" && rolledTo.attr("class") != "dropdownTrigger") {
                     off($trigger, target);
                 }
             });
         }
     });
+
+    // Gallery Plugin
+    if (jQuery("div#foo2").length > 0) {
+        jQuery("div#foo2").carouFredSel({
+            circular: false,
+            infinite: false,
+            auto: false,
+            prev: {
+                button: "#foo2_prev",
+                key: "left"
+            },
+            next: {
+                button: "#foo2_next",
+                key: "right"
+            },
+            pagination: "#foo2_pag"
+        });
+
+        jQuery("div.image_carousel div#foo2 img").changeHero();
+    }
 
     modernizr();
 });
--- js/denihan.baconbar.toggler.js
+++ js/denihan.baconbar.toggler.js
@@ -4,8 +4,8 @@
         var settings = $.extend({
             defaultMode: "off",
             target: "div.bookingengine div.best_rate_promise",
-            onMode: function($trigger,target) { $(target).animate({ opacity: 1 }, { duration: 200 }); $trigger.attr("data-denihan-mode", "on"); },
-            offMode: function($trigger,target) { $(target).animate({ opacity: 0 }, { duration: 200 }); $trigger.attr("data-denihan-mode", "off"); },
+            onMode: function($trigger,target) { $(target).css("display", "block"); $(target).animate({ opacity: 1 }, { duration: 200 }); $trigger.attr("data-denihan-mode", "on"); $("header#primary nav div a#about_menu").addClass("mouseover"); },
+            offMode: function($trigger,target) { $(target).animate({ opacity: 0 }, { duration: 200 }); $trigger.attr("data-denihan-mode", "off"); $(target).css("display", "none"); $("header#primary nav div a#about_menu").removeClass("mouseover"); },
             additionalTrigger: function(on,off,target,$trigger) { }
         }, options);
 
