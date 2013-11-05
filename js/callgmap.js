dce60f6a26d6b7685bfb43bb459b8d1a72622030
Brian Katchmar <brian.katchmar@denihan.com>
2013-11-05 17:38:37 -0500

    Touch Up
    I inspected Andreas Work, overall, good. I looked at each page in
    Chrome's Console window and found some errors which I wanted to correct.
    In addition, I went ahead and put the gmap stuff in a separate file.
    Probably should go ahead and put a plugin for that so we can use it on
    other sites, but much the reason why Andrea had to do so much work,
    Florida Cookery really tied my time up.

---
+++ js/callgmap.js
@@ -0,0 +1,20 @@
+﻿jQuery(document).ready(function () {
+    jQuery('#googlemap').gmap3({
+        map: {
+            options: {
+                streetViewControl: false,
+                mapTypeControl: false,
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.LEFT_CENTER
+                },
+                zoom: 16, center: [41.89287, -87.62543]
+            }
+        },
+        marker: {
+            latLng: [41.89287, -87.62543],
+            options: {
+                icon: new google.maps.MarkerImage("img/bbb_marker.png")
+            }
+        }
+    });
+});
--- js/denihan.baconbar.page.load.js
+++ js/denihan.baconbar.page.load.js
@@ -153,24 +153,3 @@ function preload(arrayOfImages) {
         new Image().src = this;
     });
 }
-
-$(function(){
-	$('#googlemap').gmap3({
-	map:{
-		options:{
-				streetViewControl: false,
-				mapTypeControl: false,
-				zoomControlOptions: {
-					position: google.maps.ControlPosition.LEFT_CENTER
-				},
-			zoom: 16, center:[41.89287,-87.62543]
-		}
-	},
-	marker:{
-		latLng: [41.89287,-87.62543],
-		options: {
-				icon: new google.maps.MarkerImage("img/bbb_marker.png")
-			}
-	}
-});
-});
