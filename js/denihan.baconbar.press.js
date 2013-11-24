9eb64571146bf088a7006ec6beb6c80ab14d7393
Brian Katchmar <brian.katchmar@gmail.com>
2013-11-24 17:49:33 -0500

    Work over weekend

---
+++ js/denihan.baconbar.press.js
@@ -0,0 +1,7 @@
+﻿jQuery(document).ready(function () {
+    jQuery("main#page_content article.press section header img").each(function () {
+        if (jQuery(this).attr("data-denihan-height")) {
+            jQuery(this).css("top", jQuery(this).attr("data-denihan-height") + "px");
+        }
+    });
+});
