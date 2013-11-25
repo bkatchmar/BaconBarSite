b3aee40e7e45166d9649f22028d8edbba13103b5
Brian Katchmar <brian.katchmar@denihan.com>
2013-11-25 17:27:40 -0500

    Time Sensitive Code

---
+++ js/denihan.baconbar.time.js
@@ -0,0 +1,12 @@
+﻿jQuery(document).ready(function () {
+    var currentHour = new Date().getHours();
+
+    if (currentHour >= 6 && currentHour <= 18) {
+        var imgLocation = "img/hero-home-day.jpg";
+        jQuery("main#page_content div.hero img.main").attr("src", imgLocation);
+    }
+    else {
+        var imgLocation = "img/hero-home-night.jpg";
+        jQuery("main#page_content div.hero img.main").attr("src", imgLocation);
+    }
+});
